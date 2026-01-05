8f07b78f40a768f50dd90a1bafe75da1847ef735
René Fritze <rene.fritze@arup.com>
2026-01-05 17:31:00 +0100

    optionally load .env for testing set up vars

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/hashicorp/terraform-plugin-framework v0.10.0
 	github.com/hashicorp/terraform-plugin-go v0.13.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.20.0
+	github.com/joho/godotenv v1.5.1
 	github.com/reinoudk/go-sonarcloud v0.3.4
 )
 
--- sonarcloud/provider_test.go
+++ sonarcloud/provider_test.go
@@ -1,11 +1,14 @@
 package sonarcloud
 
 import (
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"os"
+	"path/filepath"
+	"runtime"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/joho/godotenv"
 )
 
 var testAccProviderFactories map[string]func() (tfprotov6.ProviderServer, error)
@@ -16,7 +19,30 @@ func init() {
 	}
 }
 
+func repoRoot(t *testing.T) string {
+	_, file, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("Cannot determine repository root")
+	}
+	dir := filepath.Dir(file)        // directory containing this file
+	parent := filepath.Dir(dir)      // one directory up
+	abs, err := filepath.Abs(parent) // normalize to absolute path
+	if err != nil {
+		t.Fatal(err)
+	}
+	return abs
+}
 func testAccPreCheck(t *testing.T) {
+	// get config variables either from a `.env` file or process env
+	envFile := filepath.Join(repoRoot(t), ".env")
+	if _, err := os.Stat(envFile); err == nil {
+		err := godotenv.Load(envFile)
+		if err != nil {
+			errMsg := "Could not load .env" + envFile + " file from repo root: " + err.Error()
+			t.Error(errMsg)
+			t.Fatal("Error loading .env file from repo root")
+		}
+	}
 	if v := os.Getenv("SONARCLOUD_ORGANIZATION"); v == "" {
 		t.Fatal("SONARCLOUD_ORGANIZATION must be set for acceptance tests")
 	}
