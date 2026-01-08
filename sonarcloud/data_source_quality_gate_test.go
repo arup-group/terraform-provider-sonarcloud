ce52fb77de8b2080e1af62400e96504a331f1a3c
René Fritze <rene.fritze@arup.com>
2026-01-08 13:26:59 +0100

    re-enable test for gate selection data

--- sonarcloud/data_source_quality_gate_test.go
+++ sonarcloud/data_source_quality_gate_test.go
@@ -4,35 +4,43 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func testDataAccPreCheckQualityGate(t *testing.T) {
-	if v := os.Getenv("SONARCLOUD_QUALITY_GATE_NAME"); v == "" {
-		t.Fatal("SONARCLOUD_QUALITY_GATE_NAME must be set for acceptance tests")
+	if v := os.Getenv("SONARCLOUD_PROJECT_KEY"); v == "" {
+		t.Fatal("SONARCLOUD_PROJECT_KEY must be set for acceptance tests")
 	}
 }
 
-// func TestAccDataSourceQualityGate(t *testing.T) {
-// 	qualityGateName := os.Getenv("SONARCLOUD_QUALITY_GATE_NAME")
-//
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:                 func() { testAccPreCheck(t); testDataAccPreCheckQualityGate(t) },
-// 		ProtoV6ProviderFactories: testAccProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccDataSourceQualityGateConfig(qualityGateName),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttr("data.sonarcloud_quality_gate.test_quality_gate", "name", qualityGateName),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+func TestAccDataSourceQualityGate(t *testing.T) {
+	// create a unique name for the quality gate resource used by the test
+	name := fmt.Sprintf("tf-acceptance-qg-%d", time.Now().Unix())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t); testDataAccPreCheckQualityGate(t) },
+		ProtoV6ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceQualityGateConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.sonarcloud_quality_gate.test_quality_gate", "name", name),
+				),
+			},
+		},
+	})
+}
 
 func testAccDataSourceQualityGateConfig(qualityGateName string) string {
 	return fmt.Sprintf(`
+resource "sonarcloud_quality_gate" "test_quality_gate" {
+  name = "%s"
+}
+
 data "sonarcloud_quality_gate" "test_quality_gate" {
-	name = "%s"
+  name = sonarcloud_quality_gate.test_quality_gate.name
 }
 `, qualityGateName)
 }
