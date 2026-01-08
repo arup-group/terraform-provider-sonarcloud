0bc9d3d0b8fc66b15a41bf5025b8f5885cbbf9f7
René Fritze <rene.fritze@arup.com>
2026-01-08 13:09:12 +0100

    re-enable links data source
    and make sure a resource actually exists

--- sonarcloud/data_source_project_links_test.go
+++ sonarcloud/data_source_project_links_test.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
+
+const testProjectLinkURL = "https://example.com"
 
 func testAccPreCheckDataSourceProjectLinks(t *testing.T) {
 	if v := os.Getenv("SONARCLOUD_PROJECT_KEY"); v == "" {
@@ -12,33 +16,37 @@ func testAccPreCheckDataSourceProjectLinks(t *testing.T) {
 	}
 }
 
-// func TestAccDataSourceProjectLinks(t *testing.T) {
-// 	expectedNumberOfLinks := "1"
+func TestAccDataSourceProjectLinks(t *testing.T) {
+	project := os.Getenv("SONARCLOUD_PROJECT_KEY")
 
-// 	project := os.Getenv("SONARCLOUD_PROJECT_KEY")
-
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:                 func() { testAccPreCheck(t); testAccPreCheckDataSourceProjectLinks(t) },
-// 		ProtoV6ProviderFactories: testAccProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccDataSourceProjectLinksConfig(project),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttr("data.sonarcloud_project_links.test", "links.#", expectedNumberOfLinks),
-// 					resource.TestCheckResourceAttrSet("data.sonarcloud_project_links.test", "links.0.id"),
-// 					resource.TestCheckResourceAttrSet("data.sonarcloud_project_links.test", "links.0.name"),
-// 					resource.TestCheckResourceAttrSet("data.sonarcloud_project_links.test", "links.0.type"),
-// 					resource.TestCheckResourceAttrSet("data.sonarcloud_project_links.test", "links.0.url"),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
-
-func testAccDataSourceProjectLinksConfig(projectKey string) string {
-	return fmt.Sprintf(`
-data "sonarcloud_project_links" "test" {
-	project_key = "%s"
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t); testAccPreCheckDataSourceProjectLinks(t) },
+		ProtoV6ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceProjectLinksConfigWithLink(project, testProjectLinkURL),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.sonarcloud_project_links.test", "links.#", "1"),
+					resource.TestCheckResourceAttrSet("data.sonarcloud_project_links.test", "links.0.id"),
+					resource.TestCheckResourceAttr("data.sonarcloud_project_links.test", "links.0.name", "Terraform Test Link"),
+					resource.TestCheckResourceAttr("data.sonarcloud_project_links.test", "links.0.url", testProjectLinkURL),
+				),
+			},
+		},
+	})
 }
-`, projectKey)
+
+func testAccDataSourceProjectLinksConfigWithLink(projectKey, linkURL string) string {
+	return fmt.Sprintf(`
+resource "sonarcloud_project_link" "test" {
+  project_key = "%[1]s"
+  name        = "Terraform Test Link"
+  url         = "%[2]s"
+}
+
+data "sonarcloud_project_links" "test" {
+	project_key = "%[1]s"
+	depends_on = [sonarcloud_project_link.test]
+}
+`, projectKey, linkURL)
 }
